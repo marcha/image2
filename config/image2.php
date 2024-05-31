7d23c9648c119986485caddad0ac27421e6a6075
Nikola Marčić <marcic74@gmail.com>
2024-05-31 00:04:49 +0000

    sredjivanje strukture fodldera

--- config/image2.php
+++ config/image2.php
@@ -2,5 +2,30 @@
 
 // config for Marcha/Image2
 return [
+	/**
+	 * the image worker, must be "Gd" or "Imagick"
+	 */
+	'worker' => 'Gd',
 
+	'route' => '/_img',
+
+	/**
+	 * note this is the server path to the file
+	 * from base_path()
+	 */
+	'js_path' => '/public/js/Imagecow.js',
+
+	/**
+	 * various $_GET variables
+	 */
+	'vars' =>  [
+		'image'           => 'img',
+		'responsive_flag' => 'responsive',
+		'transform'       => 'transform'
+    ],
+
+	'cache' => [
+		'lifetime' => 1,
+		'path'     => 'images' // /app/storage/cache/{images}
+    ]
 ];
